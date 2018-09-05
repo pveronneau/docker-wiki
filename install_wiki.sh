3aec4e807abdf4cdaf7d957a8495247ea5431a94
Patrick Veronneau <patrick@decepticons.ca>
2018-09-04 19:58:32 -0600

    change install

--- install_wiki.sh
+++ install_wiki.sh
@@ -5,7 +5,8 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 echo "Enter the desired SQL root password:"
-read -s MYSQL_ROOT_PASSWORD
+read -s TYPED_PASS
+export MYSQL_ROOT_PASSWORD=$TYPED_PASS
 # Increase map count for elastic search
 sysctl -w vm.max_map_count=262144
 cat << EOF > /usr/lib/sysctl.d/70-elastic.conf 
